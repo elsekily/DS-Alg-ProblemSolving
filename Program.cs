94115fa04279c7b7fcb07d29e12673ccd56b274b
elsekily <youssefelsekily@outlook.com>
2022-04-24 16:25:06 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DS_Alg_ProblemSolving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
